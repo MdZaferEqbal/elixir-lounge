24ac03e81552d8b49366b59808387151782516c3
Md Zafer Eqbal <mdzafereqbal@gmail.com>
2025-06-25 22:58:27 +0530

    final-commit

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,5 +1,57 @@
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="min-h-[200vh]">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+      expedita ullam eaque reprehenderit quaerat repellendus, perferendis natus
+      doloribus alias eum tempora. Rem qui eos cupiditate velit? Vel odit
+      laboriosam laborum. Veritatis nisi, saepe incidunt sunt similique dolores
+      ex dolorem ratione asperiores provident, reiciendis placeat explicabo
+      nesciunt reprehenderit enim autem voluptas, distinctio accusamus facere
+      ullam aspernatur. Neque corporis odio eligendi nesciunt? Porro facere
+      eveniet commodi, laboriosam atque quo facilis odio nisi laborum, ipsam
+      itaque, pariatur labore tempore deleniti nemo earum veniam dolorem? Eaque
+      repellat laudantium voluptatum ex adipisci perferendis fuga nemo. Ipsam
+      similique assumenda temporibus obcaecati voluptatibus, possimus
+      dignissimos vero voluptates eveniet maiores expedita, amet aspernatur
+      explicabo facilis impedit doloremque, blanditiis optio odio corrupti quasi
+      delectus repudiandae. Dignissimos cupiditate minima animi. Doloribus
+      numquam consectetur accusantium reiciendis quibusdam pariatur mollitia
+      ducimus praesentium nam inventore illum alias vel iste, repellendus eaque
+      unde, est magnam a hic harum voluptate non! Saepe, voluptatem omnis!
+      Culpa? Aliquam quam voluptatum numquam eveniet quibusdam, expedita tempore
+      vero minus asperiores eum esse recusandae distinctio totam at soluta ipsa!
+      Corrupti cupiditate non vitae porro saepe laboriosam aliquam officia optio
+      possimus. Voluptas tempora, a ullam eius laudantium autem praesentium,
+      consequatur tenetur sed itaque rerum libero numquam eos deserunt molestiae
+      ipsa ut nihil! Ut, atque modi? Voluptates quisquam odio iusto dolores
+      adipisci? Similique obcaecati cumque fugiat quam natus. In eius quas
+      commodi repudiandae repellat iste alias cupiditate unde fuga accusantium
+      impedit, laborum numquam dicta eaque saepe consequuntur, minima est, nulla
+      voluptas. Ab! Velit fugit error, quidem odit quaerat ducimus sunt aut
+      rerum nesciunt nam dignissimos vel. Explicabo cumque tempore quo,
+      voluptatum quibusdam atque adipisci molestiae modi laborum dolore ad
+      porro, placeat illum. Id, mollitia corrupti, provident earum maiores eaque
+      culpa, rerum illo ipsum dolorem porro. Sint qui aliquid magnam, eius
+      tempora repudiandae molestiae dicta sunt provident ratione, quaerat
+      reiciendis aperiam porro fugiat. Natus, enim eum! Libero cupiditate
+      assumenda reprehenderit possimus ullam excepturi doloremque!
+      Exercitationem, quo fugit, odit maxime quis ipsum est facere minima eaque
+      officia, vero illum dolorum voluptates unde iusto voluptate. Voluptates
+      consectetur nam, placeat, vitae neque voluptas eius perferendis assumenda,
+      quia dolore corrupti quaerat officiis esse. Pariatur, deserunt quia, magni
+      omnis temporibus maiores illum nihil, obcaecati asperiores deleniti
+      voluptatibus quas. Id, tempora! Iste cum porro, optio nostrum iusto eius
+      quisquam aliquam voluptates sunt consequuntur laudantium ea accusamus
+      nobis provident ut, odit, quod ullam similique! Deleniti molestiae ducimus
+      ex tenetur ut. Officia laborum amet dolores consequuntur minima, neque
+      repudiandae delectus culpa vero quae ducimus tenetur explicabo possimus
+      quisquam est impedit, nemo perferendis sint ipsa tempore beatae unde earum
+      debitis voluptatem? Accusamus! Consequatur provident sed repudiandae
+      repellendus impedit dolorum at totam quisquam minima deleniti incidunt,
+      consectetur eos nostrum facere? Cum asperiores expedita optio nobis amet
+      quo dolorem, eveniet eaque corrupti animi totam.
+    </section>
+  );
 };
 
 export default Hero;
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { logo } from "../utils";
+import { navLinks } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Navbar = () => {
+  useGSAP(() => {
+    const headerTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#el-header",
+        start: "bottom top",
+        end: "top top",
+        scrub: 0.75,
+      },
+    });
+
+    headerTween.fromTo(
+      "#el-header",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000050",
+        backdropFilter: "blur(10px)",
+      }
+    );
+  }, []);
+
   return (
-    <header>
-      <nav>Navbar</nav>
+    <header id="el-header" className="fixed z-50 w-full">
+      <nav className="flex md:flex-row md:w-[90%] flex-col md:justify-between items-center gap-5 py-5 lg:px-0 px-5 container mx-auto">
+        <Link
+          href="#hero"
+          className="flex items-center gap-2 cursor-pointer text-nowrap md:text-base text-sm"
+        >
+          <Image src={logo} alt="Elixir Lounge Logo" placeholder="blur" />
+          <p className="font-modern-negra text-3xl -mb-2">Elixir Lounge</p>
+        </Link>
+        <ul className="flex justify-center items-center lg:gap-12 gap-7">
+          {navLinks.map((navLink) => (
+            <li key={navLink.id}>
+              <Link
+                href={`#${navLink.id}`}
+                className="cursor-pointer text-nowrap md:text-base text-sm hover:text-[#933ece] transition-all"
+              >
+                {navLink.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
