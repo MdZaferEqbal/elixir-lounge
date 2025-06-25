5a3127909566bb9eadbc2a9bb2693be95e3ca0dd
Md Zafer Eqbal <mdzafereqbal@gmail.com>
2025-06-25 20:40:06 +0530

    Initial Commit

---
+++ src/app/utils/index.js
@@ -0,0 +1,86 @@
+import abt1 from "../../../public/images/abt1.png";
+import abt2 from "../../../public/images/abt2.png";
+import abt3 from "../../../public/images/abt3.png";
+import abt4 from "../../../public/images/abt4.png";
+import abt5 from "../../../public/images/abt5.png";
+
+import arrow from "../../../public/images/arrow.png";
+import check from "../../../public/images/check.png";
+
+import cocktailLeftLeaf from "../../../public/images/cocktail-left-leaf.png";
+import cocktailRightLeaf from "../../../public/images/cocktail-right-leaf.png";
+
+import cup2 from "../../../public/images/cup-2.png";
+
+import drink1 from "../../../public/images/drink1.png";
+import drink2 from "../../../public/images/drink2.png";
+import drink3 from "../../../public/images/drink3.png";
+import drink4 from "../../../public/images/drink4.png";
+
+import favicon from "../../../public/images/fav.png";
+
+import fb from "../../../public/images/fb.png";
+import insta from "../../../public/images/insta.png";
+import x from "../../../public/images/x.png";
+
+import footerDrinks from "../../../public/images/footer-drinks.png";
+import footerLeftLeaf from "../../../public/images/footer-left-leaf.png";
+import footerRightLeaf from "../../../public/images/footer-right-leaf.png";
+
+import heroLeftLeaf from "../../../public/images/hero-left-leaf.png";
+import heroRightLeaf from "../../../public/images/hero-right-leaf.png";
+
+import leftArrow from "../../../public/images/left-arrow.png";
+import rightArrow from "../../../public/images/right-arrow.png";
+
+import logo from "../../../public/images/logo.png";
+import maskImg from "../../../public/images/mask-img.png";
+import noise from "../../../public/images/noise.png";
+
+import profile1 from "../../../public/images/profile1.png";
+import profile2 from "../../../public/images/profile2.png";
+import profile3 from "../../../public/images/profile3.png";
+import profile4 from "../../../public/images/profile4.png";
+
+import sliderLeftLeaf from "../../../public/images/slider-left-leaf.png";
+import sliderRightLeaf from "../../../public/images/slider-right-leaf.png";
+
+import underImg from "../../../public/images/under-img.jpg";
+
+export {
+  abt1,
+  abt2,
+  abt3,
+  abt4,
+  abt5,
+  arrow,
+  check,
+  cocktailLeftLeaf,
+  cocktailRightLeaf,
+  cup2,
+  drink1,
+  drink2,
+  drink3,
+  drink4,
+  favicon,
+  fb,
+  insta,
+  x,
+  footerDrinks,
+  footerLeftLeaf,
+  footerRightLeaf,
+  heroLeftLeaf,
+  heroRightLeaf,
+  leftArrow,
+  rightArrow,
+  logo,
+  maskImg,
+  noise,
+  profile1,
+  profile2,
+  profile3,
+  profile4,
+  sliderLeftLeaf,
+  sliderRightLeaf,
+  underImg,
+};
