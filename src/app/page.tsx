0acf6deabc220281338e3fcb696e7ac8c0668616
Md Zafer Eqbal <mdzafereqbal@gmail.com>
2025-06-28 18:08:41 +0530

    v/1.0.0-About-Section-Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Cocktails, Hero } from "./components";
+import { About, Cocktails, Hero } from "./components";
 
 export default function Home() {
   return (
     <main className="w-full overflow-x-hidden">
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 }
